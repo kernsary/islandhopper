9d717180c99b2f646eb84c2477bbe0fbc009f934
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-11-01 14:15:35 +0000

    add client

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>hi friends!</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+</style>
