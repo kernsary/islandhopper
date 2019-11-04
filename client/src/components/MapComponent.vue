43923b0221247001113a8fd28226f86bb7bf10de
Michael Alexander Robertson <MJCRobertson@outlook.com>
2019-11-04 14:30:22 +0000

    Attempting to add in for loop for islands lat and long

--- client/src/components/MapComponent.vue
+++ client/src/components/MapComponent.vue
@@ -1,9 +1,9 @@
 <template lang="html">
 
-<div id="mapid">
-  <LMap ref="myMap">
-      </LMap>
-</div>
+  <div id="mapid">
+    <LMap ref="myMap">
+    </LMap>
+  </div>
 
 </template>
 
@@ -14,30 +14,41 @@ export default {
   name: 'map-component',
   props: ['islands'],
   components: {
-        LMap,
-        LTileLayer,
-        LMarker
-    },
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+
+  methods: {
+    islandLabels = function (islands) {
+
+      islands.forEach((island) => {
+        console.log(island.lat)
+        L.circle([island.lat, island.long], {
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.5,
+          radius: 2500
+        }).addTo(this.myMap);
+      });
+    }
+  },
 
   mounted() {
     this.myMap = L.map('mapid').setView([58, -4], 6);
 
 
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 20,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoibWFkbWlrZTcxNiIsImEiOiJjazJrOWN4N2IwMzk5M21udDRzanZvNnFuIn0.q-CVhpCwm3WYFcsh-xlIVw'
-}).addTo(this.myMap);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 20,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoibWFkbWlrZTcxNiIsImEiOiJjazJrOWN4N2IwMzk5M21udDRzanZvNnFuIn0.q-CVhpCwm3WYFcsh-xlIVw'
+    }).addTo(this.myMap);
 
-  L.circle([57.267, -7.317], {
-	color: 'red',
-	fillColor: '#f03',
-	fillOpacity: 0.5,
-	radius: 2500
-}).addTo(this.myMap);
-
+    this.islandLabels(this.islands);
   }
+
+
 
 }
 
