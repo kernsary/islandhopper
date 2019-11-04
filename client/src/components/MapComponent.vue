892e83fe62f5d6033364074db44d3212fbdeb475
Michael Alexander Robertson <MJCRobertson@outlook.com>
2019-11-04 10:16:15 +0000

    added in access key for leaflet

--- client/src/components/MapComponent.vue
+++ client/src/components/MapComponent.vue
@@ -14,7 +14,6 @@ export default {
     'islands-in-region-list': IslandRegionsList
   }
 }
-</script>
 
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
@@ -22,8 +21,12 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	id: 'mapbox.streets',
-	accessToken: 'your.mapbox.access.token'
+	accessToken: 'pk.eyJ1IjoibWFkbWlrZTcxNiIsImEiOiJjazJrOWN4N2IwMzk5M21udDRzanZvNnFuIn0.q-CVhpCwm3WYFcsh-xlIVw'
 }).addTo(mymap);
+
+
+</script>
+
 
 <style lang="css" scoped>
 
