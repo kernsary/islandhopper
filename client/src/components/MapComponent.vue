b36014a8a21deb41fa5b9c6ad97e09c34d61fba4
Michael Alexander Robertson <MJCRobertson@outlook.com>
2019-11-04 10:08:38 +0000

    Added the map intop the component - non functional

--- client/src/components/MapComponent.vue
+++ client/src/components/MapComponent.vue
@@ -1,5 +1,9 @@
 <template lang="html">
 
+<div id="mapid">
+
+</div>
+
 </template>
 
 <script>
@@ -12,5 +16,19 @@ export default {
 }
 </script>
 
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
 <style lang="css" scoped>
+
+#mapid {
+  height: 280px;
+}
+
 </style>
